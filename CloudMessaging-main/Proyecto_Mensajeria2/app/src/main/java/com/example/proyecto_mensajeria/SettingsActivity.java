bc024efe59f7595c67c3c7672f104f257af92e8d
LuisFernando72 <lpaxelc@miumg.edu.gt>
2022-03-02 22:35:50 -0600

    Androiddd

---
+++ CloudMessaging-main/Proyecto_Mensajeria2/app/src/main/java/com/example/proyecto_mensajeria/SettingsActivity.java
@@ -0,0 +1,199 @@
+package com.example.proyecto_mensajeria;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private Button UpdateAccountSettings;
+    private EditText userName, userStatus;
+    private CircleImageView userProfileImage;
+    private String currentUserID;
+    private FirebaseAuth mAuth;
+    private DatabaseReference RootRef;
+    private static final int GalleryPick = 1;
+    private StorageReference UserProfileImageRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        RootRef = FirebaseDatabase.getInstance().getReference();
+        UserProfileImageRef = FirebaseStorage.getInstance().getReference().child("Profile Images");
+
+        InitializeFields();
+
+
+        userName.setVisibility(View.INVISIBLE);
+        UpdateAccountSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UpdateSettings();
+            }
+        });
+        RetrieveUserInfo();
+
+        userProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                //      startActivityForResult(galleryIntent, GalleryPick);
+                startActivityForResult(galleryIntent, GalleryPick);
+            }
+        });
+    }
+
+
+    private void InitializeFields() {
+        UpdateAccountSettings = (Button) findViewById(R.id.update_settings_button);
+        userName = (EditText) findViewById(R.id.set_user_name);
+        userStatus = (EditText) findViewById(R.id.set_profile_status);
+        userProfileImage = (CircleImageView) findViewById(R.id.set_profile_image);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GalleryPick && resultCode == RESULT_OK && data != null) {
+            Uri ImageUri = data.getData();
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1, 1)
+                    .start(this);
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                StorageReference filePath = UserProfileImageRef.child(currentUserID + ".jpg");
+
+
+                filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        public void onComplete (@NonNull Task < UploadTask.TaskSnapshot > task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(SettingsActivity.this, "Profile Image Succesfully", Toast.LENGTH_SHORT).show();
+
+
+                            } else {
+                                String message = task.getException().toString();
+                                Toast.makeText(SettingsActivity.this, "Error!! " + message, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                });
+
+            }
+        }
+    }
+
+    private void UpdateSettings() {
+        String setUserName = userName.getText().toString();
+        String setStatus = userStatus.getText().toString();
+
+        if (TextUtils.isEmpty(setUserName)) {
+            Toast.makeText(this, "Por favor, escribe tu nombre de usuario.", Toast.LENGTH_SHORT).show();
+        }
+
+        if (TextUtils.isEmpty(setStatus)) {
+            Toast.makeText(this, "Por favor, escribe tu estado.", Toast.LENGTH_SHORT).show();
+        } else {
+            HashMap<String, String> profileMap = new HashMap<>();
+            profileMap.put("uid", currentUserID);
+            profileMap.put("name", setUserName);
+            profileMap.put("status", setStatus);
+            RootRef.child("Users").child(currentUserID).setValue(profileMap)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                SendUserToMainActivity();
+                                Toast.makeText(SettingsActivity.this, " El Perfil ha sido actualizado!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                String message = task.getException().toString();
+                                Toast.makeText(SettingsActivity.this, "Error..." + message, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+
+        }
+
+
+    }
+
+    private void RetrieveUserInfo() {
+        RootRef.child("Users").child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if ((snapshot.exists()) && (snapshot.hasChild("name") && (snapshot.hasChild("image")))) {
+                    String retrieveUserName = snapshot.child("name").getValue().toString();
+                    String retrieveStatus = snapshot.child("status").getValue().toString();
+                    String retrieveProfileImage = snapshot.child("image").getValue().toString();
+
+                    userName.setText(retrieveUserName);
+                    userStatus.setText(retrieveStatus);
+
+                } else if ((snapshot.exists()) && (snapshot.hasChild("name"))) {
+                    String retrieveUserName = snapshot.child("name").getValue().toString();
+                    String retrieveStatus = snapshot.child("status").getValue().toString();
+
+
+                    userName.setText(retrieveUserName);
+                    userStatus.setText(retrieveStatus);
+                } else {
+                    userName.setVisibility(View.INVISIBLE);
+                    Toast.makeText(SettingsActivity.this, "Por favor actualiza la información de tu perfil...", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+    private void SendUserToMainActivity() {
+        Intent mainIntent = new Intent(SettingsActivity.this, MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
+}
